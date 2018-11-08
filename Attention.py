05ebeaf13351c12c7b043df93185f8e6c392837d
Vincent-Zh <zhangqiang@tamu.edu>
2018-11-08 11:19:35 -0600

    Attention_v4

--- Attention.py
+++ Attention.py
@@ -6,9 +6,9 @@ def Attention(inputs):
 
     attention_size = 32 #output_dim
     inputs_dim = inputs.shape[2].value
-    w_omega = tf.get_variable(tf.random_normal([inputs_dim, attention_size], stddev=0.1)) # input_dim*output_dim
-    b_omega = tf.get_variable(tf.random_normal([attention_size], stddev=0.1)) # output_dim
-    u_omega = tf.get_variable(tf.random_normal([attention_size], stddev=0.1)) # output_dim
+    w_omega = tf.get_variable("W", shape=[inputs_dim, attention_size], initializer=tf.random_normal()) # input_dim*output_dim
+    b_omega = tf.get_variable("b", shape=[attention_size], initializer=tf.random_normal()) # output_dim
+    u_omega = tf.get_variable("u", shape=[attention_size], initializer=tf.random_normal()) # output_dim
 
     v = tf.tanh(tf.tensordot(inputs, w_omega, axes=1) + b_omega) # batch*seq*output_dim
     vu = tf.tensordot(v, u_omega, axes=1, name='vu') # batch*seq
