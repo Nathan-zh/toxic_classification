2f9d84b23be9d6c323f20519c3eafe5c55bd77b2
Qiang <zhangqiang@tamu.edu>
2018-11-13 17:13:49 -0600

    5:13pm

--- baseline.py
+++ baseline.py
@@ -5,9 +5,10 @@ from keras import backend as K
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, GRU
-from keras.layers import Bidirectional, GlobalMaxPool1D
+from keras.layers import Bidirectional, GlobalMaxPool1D, GlobalAveragePooling1D
 from keras.models import Model
 from keras.utils import plot_model
+from keras.callbacks import TensorBoard
 #from keras import initializers, regularizers, constraints, optimizers, layers
 from Attention_keras import Attention
 
@@ -18,6 +19,7 @@ TEST_DATA_FILE = 'test.csv'
 embed_size = 50 # how big is each word vector
 max_features = 20000 # how many unique words to use (i.e num rows in embedding vector)
 maxlen = 100 # max number of words in a comment to use
+num = 3
 
 train = pd.read_csv(TRAIN_DATA_FILE)
 test = pd.read_csv(TEST_DATA_FILE)
@@ -71,8 +73,9 @@ x = Bidirectional(GRU(64, return_sequences=True, return_state=False, dropout=0.5
                       recurrent_dropout=0.1))(x)
 #x = Bidirectional(LSTM(32, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
 #x = Bidirectional(LSTM(32, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
-#x = GlobalMaxPool1D()(x)
-x = Attention(maxlen)(x)
+x = GlobalAveragePooling1D()(x)
+x = GlobalMaxPool1D()(x)
+#x = Attention(maxlen)(x)
 x = Dense(64, activation="relu")(x)
 x = Dropout(0.1)(x)
 x = Dense(32, activation="relu")(x)
@@ -80,8 +83,9 @@ x = Dense(6, activation="sigmoid")(x)
 model = Model(inputs=inp, outputs=x)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-history = model.fit(X_t, y, batch_size=32, epochs=2, validation_split=0.1)
-
+tensorboard = TensorBoard(log_dir='./keras_model/model{}/logs'.format(num), histogram_freq=0, write_graph=True,
+                          write_images=True)
+history = model.fit(X_t, y, batch_size=32, epochs=2, validation_split=0.1, callbacks=[tensorboard])
 
 # test
 df_test_label = pd.read_csv('test_labels.csv')
@@ -95,20 +99,20 @@ X_test = X_te[idx]
 
 score = model.evaluate(x=X_test, y=y_test, batch_size=512, verbose=1)
 
-#print(model.metrics_names)
-#print(score)
+# print(model.metrics_names)
+# print(score)
 print('*********Test accuracy is %.3f*********' % score[1])
 
-num = 2
+
 MODEL_PATH = './keras_model/model{}/'.format(num)
 
-model.save(MODEL_PATH+'model.h5')
-print("Saved model to disk %s" % MODEL_PATH)
+model.save_weights(MODEL_PATH+'model.h5')
+print("Saved weights to disk %s" % MODEL_PATH)
 
 plot_model(model, to_file=MODEL_PATH+'graph.png')
 print("Saved graph to disk %s" % MODEL_PATH)
 
-#print(history.history)
+# print(history.history)
 # Plot training & validation accuracy values
 plt.figure(1)
 plt.plot(history.history['acc'])
